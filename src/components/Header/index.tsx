47ef7b24c3f3b00562bde156583aa750f16c164e
Gabriel Souza <eugabrielf@gmail.com>
2023-03-02 16:01:25 -0300

    arrumando nomeclatura

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import Breadcrumb from "./containers/Breadcrumb";
-import Navbar from "./containers/Navbar";
+import Breadcrumb from "./containers/breadcrumb";
+import Navbar from "./containers/navbar";
 
 export default function Header() {
   return (
